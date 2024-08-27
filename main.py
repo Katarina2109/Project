064fcc7de882cbf63d25d60eac0b6775df86824e
Kateryna <katenka.vi21@gmail.com>
2024-08-27 23:40:48 +0200

    new

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import mysql.connector
+from prettytable import PrettyTable
 from local_settings import dbconfig
 from mySQL_manager import MySQLConnection
 from query_templates import *
-
 
 
 def execute_query(cursor, query, params=None):
@@ -49,7 +49,6 @@ def show_popular_keywords(cursor):
         print(f"Ошибка выполнения запроса: {e}")
 
 
-
 if __name__ == '__main__':
 
     with MySQLConnection(dbconfig) as db:
@@ -59,31 +58,57 @@ if __name__ == '__main__':
 
         while True:
             print("\nВыберите действие:")
-            print("1. Введите ключевое слово")
-            print("2. Выберите жанр фильма из предоставленных")
-            print("3. Введите год")
-            print("4. Выберите жанр фильма и введите год")
+            print("1. Поиск фильма по ключевому слову")
+            print("2. Поиск фильма по жанру")
+            print("3. Поиск фильма по году")
+            print("4. Поиск фильма по жанру и году")
             print("5. Список популярных запросов")
             print("0. Выход")
 
             choice = input("Введите номер действия: ")
 
             if choice == "1":
+                # Поиск фильма по ключевому слову
                 keyword = input("Введите ключевое слово для поиска: ")
                 insert_keyword(db.connection, db.cursor, keyword)
 
+                page = 1  # Начальная страница
+                results_per_page = 10  # Число результатов на странице
                 # Вывод фильмов по ключевому слову
                 films_query = f"%{keyword}%"
-                results = fetch_results(db.cursor, get_films_by_keyword_query, (films_query,))
-                if results:
-                    print(f"Фильмы, содержащие ключевое слово '{keyword}':")
-                    for row in results:
-                        print(row)
-                else:
-                    print(f"Нет фильмов с ключевым словом '{keyword}'.")
+                found_results = False
+
+                while True:
+                    offset = (page - 1) * results_per_page
+                    paginated_query = get_films_by_keyword_query
+
+                    results = fetch_results(db.cursor, get_films_by_keyword_query, (films_query, offset))
+
+                    if results:
+                        found_results = True
+                        print(f"\nФильмы, содержащие ключевое слово '{keyword}' на странице {page}:")
+                        table = PrettyTable(["Название фильма", "Жанр", "Год"])
+                        for row in results:
+                            title = row[0]
+                            genre = row[1]
+                            release_year = row[2]
+                            table.add_row([title, genre, release_year])
+                        print(table)
+
+                        next_page = input("\nПоказать следующую страницу? (y/n): ").strip().lower()
+                        if next_page == 'y':
+                            page += 1
+                        else:
+                            break
+                    else:
+                        if not found_results:  # Если фильмы не были найдены ни на одной из страниц
+                            print(f"\nНет фильмов с ключевым словом '{keyword}'.")
+                        else:
+                            print(f"\nЭто весь список по ключевому слову '{keyword}'.")
+                        break
 
             elif choice == "2":
-                # Выбор жанра фильма из предоставленных
+                # Поиск фильма по жанру
                 print("Доступные жанры:")
                 genres = fetch_results(db.cursor, get_category_query)
                 genre_list = [genre[0] for genre in genres]
@@ -94,28 +119,72 @@ if __name__ == '__main__':
                     genre_choice = int(input("Введите номер жанра для поиска: "))
                     if 1 <= genre_choice <= len(genre_list):
                         selected_genre = genre_list[genre_choice - 1]
-                        results = fetch_results(db.cursor, get_film_genre_query, (selected_genre,))
-                        if results:
-                            print("Фильмы с выбранным жанром:")
-                            for row in results:
-                                print(row)
-                        else:
-                            print("Нет фильмов с таким жанром.")
+
+                        page = 1  # Начальная страница
+                        results_per_page = 10  # Число результатов на странице
+
+                        while True:
+                            offset = (page - 1) * results_per_page
+
+                            results = fetch_results(db.cursor, get_film_genre_query, (selected_genre, offset))
+
+                            if results:
+                                print(f"\nФильмы с жанром '{selected_genre}' на странице {page}:")
+                                table = PrettyTable(["Название фильма", "Жанр", "Год"])
+                                for row in results:
+                                    title = row[0]
+                                    genre = row[1]
+                                    release_year = row[2]
+                                    table.add_row([title, genre, release_year])
+                                print(table)
+
+                                next_page = input("\nПоказать следующую страницу? (y/n): ").strip().lower()
+                                if next_page == 'y':
+                                    page += 1
+                                else:
+                                    break
+                            else:
+                                print(f"\nЭто весь список фильмов с жанром '{selected_genre}'.")
+                                break
                     else:
-                        print("Неверный выбор номера жанра.")
+                        print("\nНеверный выбор номера жанра.")
                 except ValueError:
-                    print("Пожалуйста, введите корректный номер жанра.")
+                    print("\nПожалуйста, введите корректный номер жанра.")
 
             elif choice == "3":
-                # Поиск фильмов по году
+                # Поиск фильма по году
                 year = input("Введите год выпуска для поиска: ")
-                results = fetch_results(db.cursor, get_film_year_query, (year,))
-                if results:
-                    print("Фильмы с выбранным годом:")
-                    for row in results:
-                        print(row)
-                else:
-                    print("Нет фильмов с таким годом.")
+
+                page = 1  # Начальная страница
+                results_per_page = 10  # Число результатов на странице
+
+                while True:
+                    offset = (page - 1) * results_per_page
+
+                    results = fetch_results(db.cursor, get_film_year_query, (year, offset))
+
+                    if results:
+                        print(f"\nФильмы с '{year}' годом на странице {page}:")
+                        table = PrettyTable(["Название фильма", "Жанр", "Год"])
+                        for row in results:
+                            title = row[0]
+                            genre = row[1]
+                            release_year = row[2]
+                            table.add_row([title, genre, release_year])
+                        print(table)
+
+                        next_page = input("\nПоказать следующую страницу? (y/n): ").strip().lower()
+                        if next_page == 'y':
+                            page += 1
+                        else:
+                            break
+
+                    else:
+                        if page == 1:
+                            print(f"\nНет фильмов с '{year}' годом.")
+                        else:
+                            print(f"\nЭто весь список фильмов с '{year}' годом.")
+                        break
 
             elif choice == "4":
                 # Поиск фильмов по жанру и году
@@ -132,27 +201,80 @@ if __name__ == '__main__':
 
                         year = input("Введите год выпуска для поиска: ")
 
-                        results = fetch_results(db.cursor, get_film_genre_year_query, (selected_genre, year))
-                        if results:
-                            print("Фильмы с выбранным жанром и годом:")
-                            for row in results:
-                                print(row)
-                        else:
-                            print("Нет фильмов с таким жанром и годом.")
+                        page = 1
+                        results_per_page = 10
+
+                        while True:
+                            offset = (page - 1) * results_per_page
+
+                            results = fetch_results(db.cursor, get_film_genre_year_query,
+                                                    (selected_genre, year, offset))
+                            if results:
+                                print(
+                                    f"\nФильмы с выбранным жанром '{selected_genre}' и '{year}' годом на странице {page}:")
+                                table = PrettyTable(["Название", "Жанр", "Год выпуска"])
+                                for row in results:
+                                    title = row[0]
+                                    genre = row[1]
+                                    release_year = row[2]
+                                    table.add_row([title, genre, release_year])
+                                print(table)
+
+                                next_page = input("\nПоказать следующую страницу? (y/n): ").strip().lower()
+                                if next_page == 'y':
+                                    page += 1
+                                else:
+                                    break
+                            else:
+                                if page == 1:  # Если нет результатов на первой странице
+                                    print(f"\nНет фильмов с жанром '{selected_genre}' и '{year}' годом.")
+                                else:
+                                    print(f"\nЭто весь список фильмов с жанром '{selected_genre}' и '{year}' годом.")
+                                break
+
                     else:
-                        print("Неверный выбор номера жанра.")
+                        print("\nНеверный выбор номера жанра.")
+
                 except ValueError:
-                    print("Пожалуйста, введите корректный номер жанра.")
+                    print("\nПожалуйста, введите корректный номер жанра.")
 
             elif choice == "5":
                 # Вывод популярных запросов
                 print("Популярные запросы:")
-                show_popular_keywords(db.cursor)
+
+                page = 1  # Начальная страница
+                results_per_page = 10  # Число результатов на странице
+
+                while True:
+                    offset = (page - 1) * results_per_page
+
+                    results = fetch_results(db.cursor, get_popular_query, (results_per_page, offset))
+                    if results:
+                        table = PrettyTable(["Ключевое слово", "Количество"])
+                        for row in results:
+                            keyword = row[0]
+                            count = row[1]
+                            table.add_row([keyword, count])
+
+                        print(f"\nРезультаты популярных запросов на странице {page}:")
+                        print(table)
+
+                        next_page = input("\nПоказать следующую страницу? (y/n): ").strip().lower()
+                        if next_page == 'y':
+                            page += 1
+                        else:
+                            break
+                    else:
+                        if page == 1:
+                            print("\nНет популярных запросов.")
+                        else:
+                            print("\nЭто весь список популярных запросов.")
+                        break
 
             elif choice == "0":
                 # Выход из программы
-                print("Выход из программы.")
+                print("\nВыход из программы.")
                 break
 
             else:
-                print("Неверный ввод. Пожалуйста, выберите номер действия из меню.")
+                print("\nНеверный ввод. Пожалуйста, выберите номер действия из меню.")
--- query_templates.py
+++ query_templates.py
@@ -1,15 +1,15 @@
 s_query = """SELECT  title       
              FROM film
              WHERE title LIKE %s
-             LIMIT 10
+             LIMIT 10 
              ;"""
 
-get_films_by_keyword_query = """SELECT f.title, c.name AS genre, f.release_year, f.description
+get_films_by_keyword_query = """SELECT f.title, c.name AS genre, f.release_year
                                FROM film f
                                LEFT JOIN film_category fc ON f.film_id = fc.film_id
                                LEFT JOIN category c ON fc.category_id = c.category_id
                                WHERE f.title LIKE %s
-                               Limit 10;"""
+                               Limit 10 OFFSET %s;"""
 
 get_film_genre_year_query = """SELECT f.title, c.name AS genre, f.release_year
                                FROM film f
@@ -17,7 +17,8 @@ get_film_genre_year_query = """SELECT f.title, c.name AS genre, f.release_year
                                LEFT JOIN category c ON fc.category_id = c.category_id
                                WHERE c.name = %s AND f.release_year = %s
                                ORDER BY f.release_year, c.name 
-                               Limit 10;
+                               Limit 10 OFFSET %s
+                               ;
                                """
 
 get_film_genre_query = """SELECT f.title, c.name AS genre, f.release_year
@@ -26,7 +27,8 @@ get_film_genre_query = """SELECT f.title, c.name AS genre, f.release_year
                           LEFT JOIN category c ON fc.category_id = c.category_id
                           WHERE c.name = %s 
                           ORDER BY f.release_year
-                          Limit 10;
+                          Limit 10 OFFSET %s
+                          ;
                           """
 
 get_film_year_query = """SELECT f.title, c.name AS genre, f.release_year
@@ -35,7 +37,8 @@ get_film_year_query = """SELECT f.title, c.name AS genre, f.release_year
                           LEFT JOIN category c ON fc.category_id = c.category_id
                           WHERE f.release_year = %s
                           ORDER BY f.release_year
-                          Limit 10;
+                          Limit 10 OFFSET %s
+                          ;
                           """
 
 get_table_keywords_query = """CREATE TABLE IF NOT EXISTS search_keywords (
@@ -45,23 +48,15 @@ get_table_keywords_query = """CREATE TABLE IF NOT EXISTS search_keywords (
                               UNIQUE (keyword)
                               );"""
 
-
 get_count_keywords_query = """INSERT INTO search_keywords (keyword, search_count)
                               VALUES (%s, 1)
                               ON DUPLICATE KEY UPDATE search_count = search_count + 1;"""
 
-
 get_popular_query = """SELECT keyword, search_count
                        FROM search_keywords
-                       ORDER BY search_count DESC;"""
-
+                       ORDER BY search_count DESC
+                       LIMIT %s OFFSET %s;
+                       """
 
 get_category_query = """SELECT name
                         FROM category;"""
-
-
-
-
-
-
-
